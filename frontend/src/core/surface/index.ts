a1e8bbf7269b867625c8cdd2e7cdec955367fb43
UnNickk76 <fandrex1@gmail.com>
2026-08-21 23:47:45 +0200

    feat(surface): expose surface core public contract

---
+++ frontend/src/core/surface/index.ts
@@ -0,0 +1,22 @@
+export { SURFACE_CONTRACTS, getSurfaceContract } from "./profiles";
+export {
+  canPresentRichDetails,
+  canUseFreeText,
+  maxTouchSteps,
+  resolveSurfaceCapability,
+} from "./policy";
+export {
+  SURFACE_CONTRACT_VERSION,
+  type CapabilityAvailability,
+  type CapabilityAvailabilitySnapshot,
+  type CapabilityPolicy,
+  type EffectiveSurfaceCapability,
+  type InteractionModeConstraints,
+  type InteractionRole,
+  type SurfaceCapability,
+  type SurfaceCapabilityPolicy,
+  type SurfaceContext,
+  type SurfaceContract,
+  type SurfaceInteractionConstraints,
+  type SurfaceKind,
+} from "./types";
